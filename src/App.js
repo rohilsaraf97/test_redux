836ec27cd006279a94f828b87c4fda8f2769f074
Rohil Saraf <72320207+rohilsaraf97@users.noreply.github.com>
2022-12-19 00:56:31 +0530

    Update App.js
    this should work, you were consfusing actions with reducers, and  dispatch, gets passed to mapDispatchToProps(). This could be really overwhelming sometimes. Try using redux/toolkit, its easier and it has amazing documentation.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED } from "./constants";
+import "./App.css";
 
-import './App.css';
-import { gatherData } from './reducers';
-
-// const mapStateToProps=state=>{
-//   return {
-//     data: state.gatherData.data,
-//     isPending: state.gatherData.isPending,
-//     error: state.gatherData.error
-//   }
-// }
-
-// const mapDispatchToProps=(dispatch)=>{
-//   return{
-//     onRequest:()=> gatherData(dispatch)
-//   }
-// }
-
-class App extends Component{
-  componentDidMount(){
-    // this.props.onRequest();
-    console.log(this.props.store);
+class App extends Component {
+  componentDidMount() {
+    this.props.requestData();
   }
 
-  render(){
-    return(
-        <div className="App">
-          <h1>hello</h1>
-        </div>
+  render() {
+    console.log(">>state", this.props.apiResponseStoredInReduxState);
+    return (
+      <div className="App">
+        <ul>
+          {this.props.apiResponseStoredInReduxState.data.map((item) => (
+            <li key={item.app_id}>
+              App id: {item.app_id} Clicks: {item.clicks}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  apiResponseStoredInReduxState: state,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    requestData: () => {
+      dispatch({ type: REQUEST_PENDING });
+      fetch(
+        "http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03"
+      )
+        .then((res) => res.json())
+        .then((data) => dispatch({ type: REQUEST_SUCCESS, payload: data }))
+        .catch((error) => dispatch({ type: REQUEST_FAILED, payload: error }));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
