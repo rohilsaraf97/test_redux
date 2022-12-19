ec9e145014725df3ce422bff3cd8255d9cc8d1d0
Rohil Saraf <rsaraf@Rohils-MacBook-Pro.local>
2022-12-19 11:47:43 +0530

    fix: use createstore and provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
-import thunkMiddleware from 'redux-thunk';
-import {createLogger} from 'redux-logger';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createLogger } from "redux-logger";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import './index.css';
-import App from './App';
-import { gatherData } from './reducers';
+import "./index.css";
+import App from "./App";
+import { gatherData } from "./reducers";
 
-const logger=createLogger();
+const logger = createLogger();
 
-const store=configureStore({
-  reducer:gatherData,
-  middleware: [thunkMiddleware, logger]
-})
+const store = createStore(
+  gatherData,
+  compose(applyMiddleware(logger), applyMiddleware(thunk))
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App store={store}/>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
